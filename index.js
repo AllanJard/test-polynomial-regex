898b148d9891475509259a3fa706aabe1e70d442
Allan Jardine <allan@datatables.net>
2024-04-10 08:28:35 +0000

    Using a variable for the first form

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 let input = '<td>mamma</td><td><strong>papa</strong></td>';
+var _re_html = /<[^>]*>/g;
 
-console.log(input.replace(/<[^>]*>/g, ''));
+console.log(input.replace(_re_html, ''));
 
 console.log(input.replace(/(<[^>]*>)/g, ''));
